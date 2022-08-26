0e6cf32b82eea627518f02ea5b84801976414a0b
a-burlakov <fln@mail.ru>
2022-08-26 17:33:39 +0500

    Добавлена обработка новых параметров командной строки: /IBConnectionString /S /F

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def add_ibname_to_1C_window(hwnd, more) -> None:
     if 'V8TopLevelFrame' not in class_name:
         return
 
-    window_text = win32gui.GetWindowText(hwnd)
+    window_text: str = win32gui.GetWindowText(hwnd)
 
     # Находим процесс, и по этому процессу ищем командную строку.
     process_id = win32process.GetWindowThreadProcessId(hwnd)[1]
@@ -25,16 +25,58 @@ def add_ibname_to_1C_window(hwnd, more) -> None:
     commandline = process.cmdline()
 
     # Ищем имя базы.
-    iter = (x for x in commandline if x.startswith('/IBName'))
-    ib_name = next(iter, '!None')
-    if ib_name != '!None':
-        ib_name = ib_name.replace('/IBName', '')
-    else:
+    ib_name = ib_name_in_commandline(commandline)
+    if ib_name is None:
         return
 
     # Устанавливаем новое имя.
-    if ib_name not in window_text:
+    if not window_text.startswith(ib_name):
         win32gui.SetWindowText(hwnd, f'{ib_name} - {window_text}')
+
+
+def ib_name_in_commandline(commandline: list) -> str | None:
+    '''
+    Ищет имя информационной базы из командной строки 1С.
+    :param commandline: Список строк, из которых состоит строка параметров процесса.
+    :return:
+    '''
+
+    # Поиск по параметру "/IBName".
+    # Вид строки: "/IBNameMYBASE".
+    iter = (x for x in commandline if x.startswith('/IBName'))
+    par = next(iter, '')
+    if par:
+        ib_name = par.replace('/IBName', '')
+        ib_name = ib_name.strip(' "\'')
+        return ib_name
+
+    # Поиск по параметру "/S".
+    # Вид строки: '/S"server-1C:1641\ZUP"'
+    iter = (x for x in commandline if x.startswith('/S') and '\\' in x)
+    par = next(iter, '')
+    if par:
+        ib_name = par.split('\\')[-1]
+        ib_name = ib_name.strip(' "\'')
+        return ib_name
+
+    # Поиск по параметру "/F"
+    # Вид строки: '/F"D:\1C_base\ZUPRAZR"'
+    iter = (x for x in commandline if x.startswith('/F') and '\\' in x)
+    par = next(iter, '')
+    if par:
+        ib_name = par.split('\\')[-1]
+        ib_name = ib_name.strip(' "\'')
+        return ib_name
+
+    # Поиск по параметру "/IBConnectionString".
+    # Вид строки: "Srvr=MYSERVER;Ref=MYBASE;".
+    iter = (x.lower() for x in commandline if 'ref=' in x.lower())
+    par = next(iter, '')
+    if par:
+        par = par.partition(';ref=')[2] # убираем всё до ";ref="
+        par = par.partition(';')[0] # убираем всё после ";"
+        ib_name = par.strip(' "\'')
+        return ib_name
 
 
 def windows_passage() -> None:
@@ -45,6 +87,12 @@ def windows_passage() -> None:
 
 
 if __name__ == '__main__':
+
+    istest = False
+    if istest:
+        windows_passage()
+        raise SystemExit('Тестовый прогон закончен.')
+
     schedule.every(2).seconds.do(windows_passage)
 
     while True:
